06ae63b8c647bc413988b267c3b0e8677b39ba19
Abilena <abthomas625@gmail.com>
2022-10-31 22:23:02 -0500

    Initial commit

---
+++ RedditLab/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RedditLab.Models;
+using System.Diagnostics;
+
+namespace RedditLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        RedditDAL api = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ViewPost()
+        {
+            RedditPost rp = api.GetPost();
+
+            return View(rp);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedditLab/Models/RedditDAL.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace RedditLab.Models
+{
+    public class RedditDAL
+    {
+        public RedditPost GetPost()
+        {
+            var client = new RestClient($"https://www.reddit.com/r/aww/.json");
+            var request = new RestRequest();
+            var response = client.GetAsync<RedditPost>(request);
+            RedditPost sp = response.Result;
+
+            return sp;
+        }
+    }
+}
